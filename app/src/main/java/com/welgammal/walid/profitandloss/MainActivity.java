3828fd4b07cb0e00962876e115881cfea7d07588
Walid <133631844+welgammal295@users.noreply.github.com>
2024-04-02 18:50:40 -0700

    Additional button layout

--- app/src/main/java/com/welgammal/walid/profitandloss/MainActivity.java
+++ app/src/main/java/com/welgammal/walid/profitandloss/MainActivity.java
@@ -3,12 +3,36 @@ package com.welgammal.walid.profitandloss;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.welgammal.walid.profitandloss.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+
+    double mRevenue = 0.0;
+    double mCostofSale = 0.0;
+    double mGrossProfit = 0.0;
+    double mOperatingExpenses = 0.0;
+    double mOtherExpenses = 0.0;
+    double mOtherIncomes = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.netIncomeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getInformationFromDisplay();
+
+            }
+        });
+    }
+    private void getInformationFromDisplay(){
+
     }
 }
