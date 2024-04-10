358feec98ba3f2a1a945ea5b6c944f8581d1c34d
Walid <133631844+welgammal295@users.noreply.github.com>
2024-04-10 03:06:18 -0700

    Added and configured action bar with logout menu.

--- app/src/main/java/com/welgammal/walid/profitandloss/MainMenu.java
+++ app/src/main/java/com/welgammal/walid/profitandloss/MainMenu.java
@@ -1,16 +1,23 @@
 package com.welgammal.walid.profitandloss;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.welgammal.walid.profitandloss.database.entities.User;
 import com.welgammal.walid.profitandloss.databinding.ActivityMainBinding;
 import com.welgammal.walid.profitandloss.databinding.ActivityMainMenuBinding;
 
@@ -22,7 +29,8 @@ public class MainMenu extends AppCompatActivity {
     static String year = "2024";
     static String month = "January";
 
-    static int loggedInUserId = -1;
+    protected static int loggedInUserId = -1;
+    private User user;
 
     public static String getYear() {
         return year;
@@ -49,6 +57,7 @@ public class MainMenu extends AppCompatActivity {
         setContentView(view);
 
         loginUser();
+        invalidateOptionsMenu();
         
         if(loggedInUserId == -1){
             Intent intent = LoginActivity.loginIntentFactory(getApplicationContext());
@@ -60,7 +69,7 @@ public class MainMenu extends AppCompatActivity {
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(MainMenu.this, "Please selected a year and a month ", Toast.LENGTH_SHORT).show();
+              //  Toast.makeText(MainMenu.this, "Please selected a year and a month ", Toast.LENGTH_SHORT).show();
             }
 
             @Override
@@ -124,9 +133,57 @@ public class MainMenu extends AppCompatActivity {
 
     private void loginUser() {
         loggedInUserId = getIntent().getIntExtra(MAIN_MENU_ACTIVITY_USER_ID, -1);
+        user = new User("Jack", "password");
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.logout_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem item = menu.findItem(R.id.logoutMenuItem);
+        item.setVisible(true);
+        item.setTitle(user.getUsername());
+        item.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(@NonNull MenuItem item) {
+
+                showLogoutDialog();
+                return false;
+            }
+        });
+
+        return true;
+    }
+    private void showLogoutDialog(){
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(MainMenu.this);
+        final AlertDialog alertDialog = alertBuilder.create();
+        alertBuilder.setMessage("Are you sure you want to log out?");
+        alertBuilder.setPositiveButton("Log out?", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                logout();
+            }
+        });
+        alertBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                alertDialog.dismiss();
+            }
+        });
+
+        alertBuilder.create().show();
+    }
+
+    private void logout() {
+        //TODO: Finish logout method
+        startActivity(LoginActivity.loginIntentFactory(getApplicationContext()));
+    }
 
     /** TODO: Pass on year and month to Main activity
  * hint: use putExtra to pass userID, year and month
