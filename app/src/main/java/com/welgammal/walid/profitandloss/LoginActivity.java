c5e96cc16bde4a6ff5a982a9a6996bf1d78c2dd3
ThisIs5am <ThisIs5am@users.noreply.github.com>
2024-04-14 22:22:45 -0700

    Created UserCreationActivity.java & activity_user_creation.xml, filled with methods

--- app/src/main/java/com/welgammal/walid/profitandloss/LoginActivity.java
+++ app/src/main/java/com/welgammal/walid/profitandloss/LoginActivity.java
@@ -1,5 +1,7 @@
 package com.welgammal.walid.profitandloss;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -8,7 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import androidx.appcompat.app.AppCompatActivity;
+
 
 import com.welgammal.walid.profitandloss.databinding.ActivityLoginBinding;
 
