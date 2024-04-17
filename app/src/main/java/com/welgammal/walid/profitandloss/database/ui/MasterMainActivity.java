c1b732ea41659f3586c3572053fe93ab15c36eb0
ThisIs5am <ThisIs5am@users.noreply.github.com>
2024-04-17 06:15:09 -0700

    Created a new main menu, many bugs, nothing linked yet

---
+++ app/src/main/java/com/welgammal/walid/profitandloss/database/ui/MasterMainActivity.java
@@ -0,0 +1,26 @@
+package com.welgammal.walid.profitandloss.database.ui;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.welgammal.walid.profitandloss.R;
+
+public class MasterMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_master_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
