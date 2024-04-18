a705d3ba5fbc7804202368f1e2b299f19702d0c5
ThisIs5am <ThisIs5am@users.noreply.github.com>
2024-04-17 19:53:19 -0700

    Worked on MasterMainActivity.java to link buttons

--- app/src/main/java/com/welgammal/walid/profitandloss/database/ui/MasterMainActivity.java
+++ app/src/main/java/com/welgammal/walid/profitandloss/database/ui/MasterMainActivity.java
@@ -7,20 +7,51 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
+import com.welgammal.walid.profitandloss.MainMenu;
 import com.welgammal.walid.profitandloss.R;
 
-public class MasterMainActivity extends AppCompatActivity {
+public class MasterMainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_master_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        Button accountsButton = findViewById(R.id.accountsButton);
+        Button reportButton = findViewById(R.id.reportButton);
+        Button exchangeButton = findViewById(R.id.exchangeButton);
+        Button profitCalcButton = findViewById(R.id.ProfitCalcButton);
+
+        accountsButton.setOnClickListener((View.OnClickListener) this);
+        reportButton.setOnClickListener((View.OnClickListener) this);
+        exchangeButton.setOnClickListener((View.OnClickListener) this);
+        profitCalcButton.setOnClickListener((View.OnClickListener) this);
+    }
+
+    //TODO: Link up activities to buttons
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()) {
+            case R.id.accountsButton:
+                intent = new Intent(this, AccountsActivity.class); // Change to AccountsActivity class
+                break;
+            case R.id.reportButton:
+                intent = new Intent(this, ReportActivity.class); // Change to ReportActivity class
+                break;
+            case R.id.exchangeButton:
+                intent = new Intent(this, ExchangeActivity.class); // Change to ExchangeActivity class
+                break;
+            case R.id.ProfitCalcButton:
+                intent = new Intent(this, MainMenu.class);
+                break;
+            default:
+                return;
+        }
+        startActivity(intent);
     }
 }
